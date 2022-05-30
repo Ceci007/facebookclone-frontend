5a52b8022be30516f8b1d7eb04c9854eee6850cc
Ceci Benitez <cecibot9@gmail.com>
2022-05-30 11:16:49 -0400

    Added readme file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 function App() {
+  const get = async () => {
+    const res = await fetch("http://localhost:8000");
+    console.log(res);
+  };
+
+  get();
+
   return <div>welcome to frontend</div>;
 }
 
