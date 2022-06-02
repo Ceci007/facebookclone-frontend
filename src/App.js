29764802142051c2c60b3ae81e5e9369bcdd7c73
Ceci Benitez <cecibot9@gmail.com>
2022-06-02 11:27:04 -0400

    Added some routes and react-router-dom settings

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/login";
+import Profile from "./pages/profile";
+import Home from "./pages/home";
+
 function App() {
-  const get = async () => {
-    const res = await fetch("http://localhost:8000");
-    console.log(res);
-  };
-
-  get();
-
-  return <div>welcome to frontend</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/login" element={<Login />} exact />
+        <Route path="/profile" element={<Profile />} exact />
+        <Route path="/" element={<Home />} exact />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
