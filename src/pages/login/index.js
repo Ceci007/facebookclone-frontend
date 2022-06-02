4c2fa1897f9bd256467863ec5c0441ca4d7e013e
Ceci Benitez <cecibot9@gmail.com>
2022-06-02 12:57:55 -0400

    started login page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,5 +1,43 @@
 import React from "react";
+import { Formik, Form } from "formik";
+import { Link } from "react-router-dom";
+import "./style.css";
 
 export default function Login() {
-  return <div>Login page...</div>;
+  return (
+    <div className="login">
+      <div className="login_wrapper">
+        <div className="login_wrap">
+          <div className="login_1">
+            <img src="icons/facebook.svg" alt="facebook logo" />
+            <span>
+              Facebook helps you connect and share with the people in your life.
+            </span>
+          </div>
+          <div className="login_2">
+            <div className="login_2_wrap">
+              <Formik>
+                {(formik) => (
+                  <Form>
+                    <input type="text" />
+                    <input type="text" />
+                    <button type="submit" className="blue_btn">
+                      Log In
+                    </button>
+                  </Form>
+                )}
+              </Formik>
+              <Link to="/forgot">Forgotten password ?</Link>
+              <div className="sign_splitter"></div>
+              <button className="blue_btn open_signup">Create Account</button>
+            </div>
+            <Link to="/">
+              <b>Create a page</b> for a celebrity, brand or business.
+            </Link>
+          </div>
+        </div>
+        <div className="register"></div>
+      </div>
+    </div>
+  );
 }
