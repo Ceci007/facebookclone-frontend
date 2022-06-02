cf723eccb3de0337fbe30ab6ee08503b3613e894
Ceci Benitez <cecibot9@gmail.com>
2022-06-02 12:12:05 -0400

    creating the redux store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers";
+import App from "./App";
 import "./index.css";
 import "./styles/icons/icons.css";
-import App from "./App";
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
