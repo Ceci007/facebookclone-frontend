808763a658adcecc656c3878e556eb7ab3e42289
Ceci Benitez <cecibot9@gmail.com>
2022-06-04 11:48:23 -0400

    Linking input with formik

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Formik, Form } from "formik";
 import { Link } from "react-router-dom";
+import LoginInput from "../../components/inputs/loginInput";
 import "./style.css";
 
+const loginInfos = {
+  email: "",
+  password: "",
+};
+
 export default function Login() {
+  const [login, setLogin] = useState(loginInfos);
+  const { email, password } = login;
+
+  const handleLoginChange = (e) => {
+    const { name, value } = e.target;
+    setLogin({ ...login, [name]: value });
+  };
+
   return (
     <div className="login">
       <div className="login_wrapper">
@@ -16,11 +30,27 @@ export default function Login() {
           </div>
           <div className="login_2">
             <div className="login_2_wrap">
-              <Formik>
+              <Formik
+                enableReinitialize
+                initialValues={{
+                  email,
+                  password,
+                }}
+              >
                 {(formik) => (
                   <Form>
-                    <input type="text" />
-                    <input type="text" />
+                    <LoginInput
+                      type="text"
+                      name="email"
+                      placeholder="Email address or phone number"
+                      onChange={handleLoginChange}
+                    />
+                    <LoginInput
+                      type="password"
+                      name="password"
+                      placeholder="Password"
+                      onChange={handleLoginChange}
+                    />
                     <button type="submit" className="blue_btn">
                       Log In
                     </button>
