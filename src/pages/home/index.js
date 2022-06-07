4c9c3ccb42400f3872899b9a0204d0f317ebc844
Ceci Benitez <cecibot9@gmail.com>
2022-06-07 12:52:08 -0400

    Added header component

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,10 @@
 import React from "react";
+import Header from "../../components/header";
 
 export default function Home() {
-  return <div>Home page...</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
