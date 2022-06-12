8614696ec0fcf317d3bdf858fb98c859690e4c69
Ceci Benitez <cecibot9@gmail.com>
2022-06-12 11:56:32 -0400

    Added left home menu

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,18 +1,15 @@
-import React, { useRef, useState } from "react";
+import React from "react";
+import { useSelector } from "react-redux";
 import Header from "../../components/header";
-import useClickOutside from "../../helpers/clickOutside";
+import LeftHome from "../../components/home/left";
 
 export default function Home() {
-  const [visible, setVisible] = useState(true);
-  const el = useRef(null);
-
-  useClickOutside(el, () => {
-    setVisible(false);
-  });
+  const { user } = useSelector((user) => ({ ...user }));
 
   return (
     <div>
       <Header />
+      <LeftHome user={user} />
     </div>
   );
 }
