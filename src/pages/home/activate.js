79d2c99aedc8188d21488f28ca2e08ffdc7fe7e3
Ceci Benitez <cecibot9@gmail.com>
2022-06-14 13:55:55 -0400

    Activate account (part 2)

--- src/pages/home/activate.js
+++ src/pages/home/activate.js
@@ -1,18 +1,60 @@
-import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 import Header from "../../components/header";
 import LeftHome from "../../components/home/left";
 import RightHome from "../../components/home/right";
 import Stories from "../../components/home/stories";
 import CreatePost from "../../components/createPost";
 import ActivateForm from "./ActivateForm";
+import Cookies from "js-cookie";
 import "./style.css";
 
 export default function Activate() {
   const { user } = useSelector((user) => ({ ...user }));
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { token } = useParams();
+
   const [success, setSuccess] = useState("");
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    activateAccount();
+  }, []);
+
+  const activateAccount = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios.post(
+        `${process.env.REACT_APP_BACKEND_URL}/activate`,
+        {
+          token,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        }
+      );
+
+      setSuccess(data.message);
+      Cookies.set("user", JSON.stringify({ ...user, verified: true }));
+      dispatch({ type: "VERIFY", payload: true });
+
+      setTimeout(() => {
+        navigate("/");
+      }, 3000);
+    } catch (error) {
+      setError(error.response.data.message);
+      setTimeout(() => {
+        navigate("/");
+      }, 3000);
+    }
+  };
 
   return (
     <div className="home">
