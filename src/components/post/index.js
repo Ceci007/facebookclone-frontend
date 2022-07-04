f476c5a2a77e99fa241dca3e07e9abe166214252
Ceci Benitez <cecibot9@gmail.com>
2022-07-04 16:06:01 -0400

    Post component (background and images)

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -42,6 +42,47 @@ export default function Post({ post }) {
           <Dots color="#828387" />
         </div>
       </div>
+      {post.background ? (
+        <div
+          className="post_bg"
+          style={{ backgroundImage: `url(${post.background})` }}
+        >
+          <div className="post_bg_text">{post.text}</div>
+        </div>
+      ) : (
+        <>
+          <div className="post_text">{post.text}</div>
+          {post.images && post.images.length && (
+            <div
+              className={
+                post.images.length === 1
+                  ? "grid_1"
+                  : post.images.length === 2
+                  ? "grid_2"
+                  : post.images.length === 3
+                  ? "grid_3"
+                  : post.images.length === 4
+                  ? "grid_4"
+                  : post.images.length >= 5 && "grid_5"
+              }
+            >
+              {post.images.slice(0, 5).map((image, i) => (
+                <img
+                  src={image.url}
+                  key={i}
+                  alt={image.url}
+                  className={`img-${i}`}
+                />
+              ))}
+              {post.images.length > 5 && (
+                <div className="more-pics-shadow">
+                  +{post.images.length - 5}
+                </div>
+              )}
+            </div>
+          )}
+        </>
+      )}
     </div>
   );
 }
