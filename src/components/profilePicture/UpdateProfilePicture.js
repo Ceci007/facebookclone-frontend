b6e61482afd9c6429ed7aa5b51ebeec5f2b8dd7c
Ceci Benitez <cecibot9@gmail.com>
2022-07-18 15:38:50 -0400

    Update profile picture (part 2)

--- src/components/profilePicture/UpdateProfilePicture.js
+++ src/components/profilePicture/UpdateProfilePicture.js
@@ -1,7 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useCallback } from "react";
+import Cropper from "react-easy-crop";
 
-export default function UpdateProfilePicture({ setImage }) {
+export default function UpdateProfilePicture({ setImage, image }) {
   const [description, setDescription] = useState("");
+  const [crop, setCrop] = useState({ x: 0, y: 0 });
+  const [zoom, setZoom] = useState(1);
+  const jumpRef = useRef(null);
+
+  const onCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
+    console.log(croppedArea, croppedAreaPixels);
+  }, []);
+
+  const zoomOut = () => {
+    jumpRef.current.stepDown();
+    setZoom(jumpRef.current.value);
+  };
+
+  const zoomIn = () => {
+    jumpRef.current.stepUp();
+    setZoom(jumpRef.current.value);
+  };
 
   return (
     <div className="postBox update_img">
@@ -20,7 +38,51 @@ export default function UpdateProfilePicture({ setImage }) {
         ></textarea>
       </div>
       <div className="update_center">
-        <div className="crooper"></div>
+        <div className="cropper">
+          <Cropper
+            image={image}
+            crop={crop}
+            zoom={zoom}
+            aspect={1 / 1}
+            cropShape="round"
+            onCropChange={setCrop}
+            onCropComplete={onCropComplete}
+            onZoomChange={setZoom}
+          />
+        </div>
+        <div className="slider">
+          <div className="slider_circle hover1" onClick={() => zoomOut()}>
+            <i className="minus_icon"></i>
+          </div>
+          <input
+            type="range"
+            min={1}
+            max={3}
+            step={0.2}
+            ref={jumpRef}
+            value={zoom}
+            onChange={(e) => setZoom(e.target.value)}
+          />
+          <div className="slider_circle hover1" onClick={() => zoomIn()}>
+            <i className="plus_icon"></i>
+          </div>
+        </div>
+      </div>
+      <div className="flex_up">
+        <button className="gray_btn">
+          <i className="crop_icon"></i> Crop photo
+        </button>
+        <button className="gray_btn">
+          <i className="temp_icon"></i> Make temporary
+        </button>
+      </div>
+      <div className="flex_p_t">
+        <i className="public_icon"></i>
+        Your profile picture is public
+      </div>
+      <div className="update_submit_wrap">
+        <div className="blue_link">Cancel</div>
+        <button className="blue_btn">Save</button>
       </div>
     </div>
   );
